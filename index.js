4399bd3810eceec48cb3a3556d6fbcd7155d20aa
cficht <chris.ficht@gmail.com>
2020-03-09 13:18:40 -0700

    made isBoolean function and tested it

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const { isNumber } = require('./lib/types.js');
+const { isNumber, isString, isBoolean } = require('./lib/types.js');
 
 console.log(isNumber('3'));
+console.log(isString('word'));
+console.log(isString(3));
+console.log(isBoolean(true));
+console.log(isBoolean(false));
+console.log(isBoolean(1));
