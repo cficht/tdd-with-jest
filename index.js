e6684df6aa37eef2809571bcdebfb27c17eb69cf
cficht <chris.ficht@gmail.com>
2020-03-09 15:09:49 -0700

    made castToString function and tested it

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const { isNumber, isString, isBoolean, isArray, isObject, isFunction } = require('./lib/types.js');
+const { isNumber, isString, isBoolean, isArray, isObject, isFunction, castToNumber, castToString } = require('./lib/types.js');
 
-// console.log(isNumber('3'));
-// console.log(isString('word'));
-// console.log(isString(3));
-// console.log(isBoolean(true));
-// console.log(isBoolean(false));
-// console.log(isBoolean(1));
-// console.log(isArray([]));
-// console.log(isArray({}));
-// console.log(isObject({}));
+console.log(isNumber('3'));
+console.log(isString('word'));
+console.log(isString(3));
+console.log(isBoolean(true));
+console.log(isBoolean(false));
+console.log(isBoolean(1));
+console.log(isArray([]));
+console.log(isArray({}));
+console.log(isObject({}));
 console.log(isFunction(isBoolean));
+
+console.log(castToNumber({}));
+console.log(castToString([]));
