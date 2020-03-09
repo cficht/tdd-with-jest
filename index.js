8f9c1926e0383e22dbb75825bb29d7ae38387fa8
cficht <chris.ficht@gmail.com>
2020-03-09 13:30:57 -0700

    made and tested isArray function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { isNumber, isString, isBoolean } = require('./lib/types.js');
+const { isNumber, isString, isBoolean, isArray } = require('./lib/types.js');
 
 console.log(isNumber('3'));
 console.log(isString('word'));
@@ -6,3 +6,5 @@ console.log(isString(3));
 console.log(isBoolean(true));
 console.log(isBoolean(false));
 console.log(isBoolean(1));
+console.log(isArray([]));
+console.log(isArray({}));
