082430066991d17d2f1255f72556d18b3b5de0b3
cficht <chris.ficht@gmail.com>
2020-03-09 14:04:07 -0700

    made isFunction function and tested it

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const { isNumber, isString, isBoolean, isArray, isObject } = require('./lib/types.js');
+const { isNumber, isString, isBoolean, isArray, isObject, isFunction } = require('./lib/types.js');
 
-console.log(isNumber('3'));
-console.log(isString('word'));
-console.log(isString(3));
-console.log(isBoolean(true));
-console.log(isBoolean(false));
-console.log(isBoolean(1));
-console.log(isArray([]));
-console.log(isArray({}));
-console.log(isObject({}));
+// console.log(isNumber('3'));
+// console.log(isString('word'));
+// console.log(isString(3));
+// console.log(isBoolean(true));
+// console.log(isBoolean(false));
+// console.log(isBoolean(1));
+// console.log(isArray([]));
+// console.log(isArray({}));
+// console.log(isObject({}));
+console.log(isFunction(isBoolean));
