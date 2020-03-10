495c00c9969c71c7fce79fa02ac53c10999d9982
cficht <chris.ficht@gmail.com>
2020-03-10 15:38:28 -0700

    created and tested the Validator

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-const { isNumber, isString, isBoolean, isArray, isObject, isFunction, castToNumber, castToString, castToBoolean } = require('./lib/types.js');
+// const { isNumber, isString, isBoolean, isArray, isObject, isFunction, castToNumber, castToString, castToBoolean } = require('./lib/types.js');
 
-console.log(isNumber('3'));
-console.log(isString('word'));
-console.log(isString(3));
-console.log(isBoolean(true));
-console.log(isBoolean(false));
-console.log(isBoolean(1));
-console.log(isArray([]));
-console.log(isArray({}));
-console.log(isObject({}));
-console.log(isFunction(isBoolean));
+const { Validator } = require('./lib/Validator.js');
 
-console.log(castToNumber({}));
-console.log(castToString([]));
-console.log(castToBoolean('false'));
+// console.log(isNumber('3'));
+// console.log(isString('word'));
+// console.log(isString(3));
+// console.log(isBoolean(true));
+// console.log(isBoolean(false));
+// console.log(isBoolean(1));
+// console.log(isArray([]));
+// console.log(isArray({}));
+// console.log(isObject({}));
+// console.log(isFunction(isBoolean));
+
+// console.log(castToNumber({}));
+// console.log(castToString([]));
+// console.log(castToBoolean('false'));
+
+
+
+
+const nameValidator = new Validator('name', {
+  type: String,
+  required: true
+});
+
+// const spot = {
+//   name: 'spot',
+//   age: 5,
+//   weight: '20 lbs'
+// };
+
+// const spotless = {
+//   age: 5,
+//   weight: '20 lbs'
+// };
+
+console.log(nameValidator.validate({ name: [2, 2] }));
